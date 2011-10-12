1aa87d58b64f4507a32543a060e777657c7254e7
Albert Bellonch <albert@itnig.net>
2011-10-12 20:07:20 +0200

    Add image stuff and short desc to episodes

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,5 +1,6 @@
 class Episode < ActiveRecord::Base
   mount_uploader :file, EpisodeUploader
+  mount_uploader :image, ImageUploader
 
   has_many :sponsorships
   has_many :sponsors, :through => :sponsorships
@@ -10,4 +11,5 @@ class Episode < ActiveRecord::Base
   validates :number, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1 }, :presence => true
   validates_presence_of :file
   validates_presence_of :permalink
+  validates_presence_of :image
 end
