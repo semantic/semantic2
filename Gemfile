109cac346e1e448f6cff2f305c86c0fe067ab2bd
Albert Bellonch <albert@itnig.net>
2011-10-12 20:39:09 +0200

    Converting to Rails 3.1

--- Gemfile
+++ Gemfile
@@ -4,4 +4,15 @@ gem 'rails', '3.1.1'
 
 gem 'carrierwave'
 
-gem 'sqlite3'
+group :assets do
+  gem 'sass-rails',   '~> 3.1.4'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'sqlite3'
+  gem 'turn', :require => false
+end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Semantic2::Application.config.session_store :cookie_store, :key => '_semantic2_session'
+Semantic2::Application.config.session_store :cookie_store, key: '_semantic2_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
