dda694440faee675bb9064e52ccd4a79fd42aad1
Albert Bellonch <albert@itnig.net>
2011-10-12 17:52:03 +0200

    Brand new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Semantic2::Application.config.secret_token = 'ce6dc0f6a64791aaaaa35de900db884d0cd3e1c9b5201d961c1b22980934cb304fc4534978294447ad256469c383fe6b9987a55313a8be7e0d8f03b82d1a79c4'
