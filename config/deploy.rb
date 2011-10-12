15c01436818a5d48c09783478d2666d8f15f7220
Roger Campos <roger@itnig.net>
2011-10-12 20:03:47 +0200

    Configuring deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,53 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+require 'bundler/capistrano'
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :rvm_ruby_string, 'default'        # Or whatever env you want it to run in.
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :application, "semantic"
+set :repository,  "git://github.com/semantic/semantic2.git"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :scm, :git
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+role :web, "braavos.itnig.net"                          # Your HTTP server, Apache/etc
+role :app, "braavos.itnig.net"                          # This may be the same as your `Web` server
+role :db,  "braavos.itnig.net", :primary => true # This is where Rails migrations will run
+
+set :branch, "master"
+set :keep_releases, 5
+set :deploy_to, "/var/www/rails/semantic.cat/current"
+set :deploy_via, :remote_cache
+
+set :user, "deployer"
+set :ssh_options, {:forward_agent => true, :port => 8622}
+on :start do
+  `ssh-add`
+end
+set :use_sudo, false
+
+set :bundle_without, [:development, :test, :mac]
+
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+namespace :symlinks do
+  desc "Make symlinks"
+  task :make, :roles => :app, :except => { :no_release => true } do
+    commands = ["config/database.yml"].map do |path|
+      "rm -rf #{release_path}/#{path} && ln -s #{shared_path}/#{path} #{release_path}/#{path}"
+    end
+
+    run <<-CMD
+      cd #{release_path} && #{commands.join(" && ")}
+    CMD
+  end
+end
+
+after "deploy:update_code", "deploy:cleanup"
+after "deploy:update_code", 'symlinks:make'
