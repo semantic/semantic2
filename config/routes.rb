0fce3a8372fbe7f6f9bbfd6de43044f9e7997897
Jordi Romero <jordi@jrom.net>
2011-10-13 20:52:15 +0200

    episodes controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Semantic2::Application.routes.draw do
 
+  resources :episodes
+
   # Session routes
   match "/auth/:provider/callback" => "sessions#create"
   match "/desconnectar" => "sessions#destroy", :as => :signout
